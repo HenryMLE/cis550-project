e3090f181e8d9937cff58aaa845c4ccb4ddbf1db
Akshat Agrawal <akshat@huntsman-ve504-0549.apn.wlan.upenn.edu>
2017-12-12 22:04:45 -0500

    search func

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,37 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+var mongoclient = require('mongodb').MongoClient
+var url = "mongodb://team:FUSIONONFIRE1@ds137826.mlab.com:37826/ritebite"
+var _db; 
+
+mongoclient.connect(url, function(err, db) {
+  assert.equal(err, null);
+  console.log('Connected to db!');
+  _db = db;
+});
+
+cur = _db.collection('recipes').find().limit(10);
+cur.each(function(err, item) {
+  if(item != null) {
+    console.log(item);
+  }
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.send('hi')
   // res.sendFile(path.join(__dirname, '../', 'app/views', 'index.html'));
   res.render('index');
   //console.log(path.join(__dirname, '../', 'views', 'index.html'));
+});
+
+router.get('/search/:query', function(req, res, next) {
+  cur = _db.collection('recipes').find({"title" : {$regex : ".*"+req.params.query+".*"}})
+  cur.toArray(function(err, docs) {
+    console.log(docs)
+  });
 });
 
 // router.get('/signin', function(req, res, next) {
