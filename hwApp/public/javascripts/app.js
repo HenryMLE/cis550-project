b80830293ae37fc6065b77273e5c0d3d71936c84
hchittela <h.chittela@gmail.com>
2017-12-14 02:36:45 -0500

    new app from hw2

---
+++ hwApp/public/javascripts/app.js
@@ -0,0 +1,71 @@
+var app = angular.module('angularjsNodejsTutorial',[]);
+app.controller('myController', function($scope, $http) {
+    $scope.message="";
+    $scope.Submit = function() {
+        var request = $http.get('/data/'+$scope.email);
+        request.success(function(data) {
+            $scope.data = data;
+            console.log(data);
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+    
+    }; 
+});
+
+// To implement "Insert a new record", you need to:
+// - Create a new controller here
+// - Create a corresponding route handler in routes/index.js
+
+app.controller('insertController', function ($scope, $http) {
+    $scope.message="";
+    $scope.Insert = function() {
+        var request = $http.post('/insertrecord', {
+            'login':$scope.login,
+            'name':$scope.name,
+            'sex':$scope.sex,
+            'RelationshipStatus':$scope.RelationshipStatus,
+            'Birthyear':$scope.Birthyear
+        });
+        request.success(function(data) {
+            console.log('successful insert');
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+    };
+});
+
+app.controller('recipesController', function($scope, $http) {
+    $scope.message="";
+    $scope.Submit = function() {
+        var request = $http.get('/search/'+$scope.query);
+        request.success(function(data) {
+            $scope.data = data;
+            console.log(data);
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+    
+    }; 
+});
+
+// app.controller('recipeController', function($scope, $http) {
+//     console.log('in recipeController');
+//     console.log($scope);
+//     console.log($routeParams.id);
+
+//     // var r_id = req.params.id;
+//     // cur = _db.collection('recipes').find({"_id": r_id})
+//     // cur.toArray(function(err, rec) {
+//     //     if (err) {
+//     //         console.log(err);
+//     //     }
+//     //     else {
+//     //         res.json(rec[0]);
+//     //         console.log(rec[0]);
+//     //     }
+//     // });
+// });
